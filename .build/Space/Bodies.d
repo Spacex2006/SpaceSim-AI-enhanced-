1a90391c2a1ff92d18b58d977859ce9bef40cad9
Tarsh Singh <tarsh006@gmail.com>
2026-07-04 03:54:37 +0530

    Clean up: enforce .gitignore and remove large files

--- .build/Space/Bodies.d
+++
@@ -1,5 +0,0 @@
-.build/Space/Bodies.o: src/Space/Bodies.cpp src/Space/Bodies.h \
- src/utilities/Vector.h src/nlohmann/json.hpp
-src/Space/Bodies.h:
-src/utilities/Vector.h:
-src/nlohmann/json.hpp:
--- .build/main.d
+++
@@ -1,6 +0,0 @@
-.build/main.o: src/main.cpp src/Space/Bodies.h src/utilities/Vector.h \
- src/Space/Forces.h src/utilities/constants.h
-src/Space/Bodies.h:
-src/utilities/Vector.h:
-src/Space/Forces.h:
-src/utilities/constants.h:
